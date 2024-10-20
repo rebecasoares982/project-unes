125281d5c8d9e36f114114d10c7837ded2904f64
Rebeca Soares <98848463+rebecasoares982@users.noreply.github.com>
2024-10-19 21:45:51 -0300

    removed

--- Homenagem - ED SHEERAN/script.js
+++
@@ -1,7 +0,0 @@
-$('#albumModal').on('show.bs.modal', function (event) {
-var button = $(event.relatedTarget);
-var spotifyLink = button.data('spotify-link');
-var modal = $(this);
-modal.find('#spotifyLink').attr('href', spotifyLink);
-});
- 
